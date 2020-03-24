a169654ae9e127e20dd222b972c7ebcea490b208
ly <514537831@qq.com>
2020-03-24 19:10:00 +0800

    添加基本页面--页面路径有问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import category from '~views/category'
+import cart from '~views/cart'
+import profile from '~views/profile'
+
+
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect:'/home'
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home
   },
+  
   {
     path: '/about',
     name: 'About',
@@ -17,11 +27,29 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/category',
+    name: 'category',
+    component: category
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: cart
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: profile
+  },
+ 
 ]
 
+
 const router = new VueRouter({
-  routes
+  routes,
+  mode:'history'
 })
 
 export default router
